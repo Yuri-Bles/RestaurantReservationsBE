91430c9797f34207b2f7282e2e4f8cf5171b5ee5
Yuri-Bles <553227@student.fontys.nl>
2026-09-04 09:59:30 +0200

    initial commit

---
+++ src/test/java/nl/fontys/restaurantreservations/RestaurantReservationsApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.fontys.restaurantreservations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantReservationsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
