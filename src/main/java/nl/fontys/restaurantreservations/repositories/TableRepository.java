86ec76b0c1a8518578f0bd8c8fb749a52f67916e
Yuri-Bles <553227@student.fontys.nl>
2026-09-09 11:02:37 +0200

    Fixed getMethod. Started on createMethod

--- src/main/java/nl/fontys/restaurantreservations/repositories/TableRepository.java
+++ src/main/java/nl/fontys/restaurantreservations/repositories/TableRepository.java
@@ -2,6 +2,7 @@ package nl.fontys.restaurantreservations.repositories;
 
 import jakarta.persistence.EntityManager;
 import nl.fontys.restaurantreservations.dtos.*;
+import nl.fontys.restaurantreservations.enums.TableStatus;
 import nl.fontys.restaurantreservations.interfaces.ITableRepository;
 import nl.fontys.restaurantreservations.models.*;
 import org.springframework.stereotype.*;
@@ -18,15 +19,20 @@ public abstract class TableRepository implements ITableRepository
         this.entityManager = entityManager;
     }
 
-    public List<TableDTO> getAllActiveTables()
-    {
-        List<TableModel> result = entityManager
-                .createQuery("SELECT t FROM TableModel t WHERE t.status = :status", TableModel.class)
-                .setParameter("status", 0)
-                .getResultList();
-
-        return result.stream()
-                .map(TableDTO::new)
-                .toList();
-    }
+//    public List<TableDTO> findAllByStatus(TableStatus status)
+//    {
+//        List<TableModel> result = entityManager
+//                .createQuery("SELECT t FROM TableModel t WHERE t.status = :status", TableModel.class)
+//                .setParameter("status", status)
+//                .getResultList();
+//
+//        if (result.isEmpty())
+//        {
+//            return null;
+//        }
+//
+//        return result.stream()
+//                .map(TableDTO::new)
+//                .toList();
+//    }
 }
--- src/main/java/nl/fontys/restaurantreservations/services/TableService.java
+++ src/main/java/nl/fontys/restaurantreservations/services/TableService.java
@@ -1,13 +1,17 @@
 package nl.fontys.restaurantreservations.services;
 
 import nl.fontys.restaurantreservations.dtos.*;
+import nl.fontys.restaurantreservations.enums.TableStatus;
 import nl.fontys.restaurantreservations.interfaces.ITableRepository;
+import nl.fontys.restaurantreservations.models.TableModel;
+import org.springframework.stereotype.Service;
 
 import java.util.*;
 
+@Service
 public class TableService
 {
-    private ITableRepository repo;
+    private final ITableRepository repo;
 
     public TableService(ITableRepository repo)
     {
@@ -16,6 +20,18 @@ public class TableService
 
     public List<TableDTO> getAllActiveTables()
     {
-        return repo.getAllActiveTables();
+        return repo.findAllByStatus(TableStatus.Active)
+                .stream()
+                .map(TableDTO::new)
+                .toList();
+    }
+
+    public void createTable(String tableNumber, Integer capacity, String statusString)
+    {
+        TableStatus status = TableStatus.valueOf(statusString);
+
+        TableModel model = new TableModel(tableNumber, capacity, status);
+
+        repo.save(model);
     }
 }
