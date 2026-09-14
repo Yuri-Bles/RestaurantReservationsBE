d7f8231deb5a5af7f5d36e75b9534a620b121351
Yuri-Bles <553227@student.fontys.nl>
2026-09-14 11:30:11 +0200

    CI Test

---
+++ src/test/java/nl/fontys/restaurantreservations/CITest.java
@@ -0,0 +1,20 @@
+package nl.fontys.restaurantreservations;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CITest
+{
+    @Test
+    public void dummyTestSucceed()
+    {
+        assertEquals(1, 1);
+    }
+
+    @Test
+    public void dummyTestFail()
+    {
+        assertEquals(1, 2);
+    }
+}
