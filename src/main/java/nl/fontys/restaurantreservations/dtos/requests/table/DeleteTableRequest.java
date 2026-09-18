e9859af7a5b3489b099aa92250f92b4f6da0dd6c
Yuri-Bles <553227@student.fontys.nl>
2026-09-18 11:50:51 +0200

    Configured linter and fixed linting issues

--- src/main/java/nl/fontys/restaurantreservations/dtos/requests/table/DeleteTableRequest.java
+++ src/main/java/nl/fontys/restaurantreservations/dtos/requests/table/DeleteTableRequest.java
@@ -1,7 +1,5 @@
 package nl.fontys.restaurantreservations.dtos.requests.table;
 
-import nl.fontys.restaurantreservations.enums.TableStatus;
-
 public record DeleteTableRequest(
         String tableNumber
 ) {
